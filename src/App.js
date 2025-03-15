f817bd577a1251a50730892d32d76726c78eec8b
jonasluis <jonasluis66@gmail.com>
2025-03-15 08:14:35 -0300

    :wastebasket: remove: Removendo arquivos não utilizados do projeto para manter a organização e atualização contínua

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import './App.css';
 import Banner from './componentes/Banner/Banner';
 
 function App() {
   return (
     <div className="App">
       <Banner/>
-      <header className="App-header">
-        
-      </header>
+      
     </div>
   );
 }
