beec45327f232d0d367cef2ca08826f574709d3f
jonasluis <jonasluis66@gmail.com>
2025-03-16 07:20:10 -0300

    :sparkles: feat: Manipulando o input do usuário e controlando estado

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -2,10 +2,14 @@ import './index.css'
 
 export default function CampoTexto(props) {
 
+    const aoDigitado = (evento) => {
+        props.aoAlterado(evento.target.value)
+    }
+
     return ( 
         <div className="campo-texto">
             <label>{props.label}</label>
-            <input required={props.obrigatorio} placeholder={`${props.placeholder}...`}/>
+            <input value={props.valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={`${props.placeholder}...`}/>
         </div>
     )
 }
