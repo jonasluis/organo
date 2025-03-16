4429c374abeb96dd6f739a141526e0216bad3fe9
jonasluis <jonasluis66@gmail.com>
2025-03-16 06:12:52 -0300

    :sparkles: feat: Lista Suspensa no Formulario

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,14 +1,28 @@
 import CampoTexto from "../CampoTexto/index.js";
+import ListaSuspensa from "../ListaSuspensa/index.js";
 import "./index.css";
 
 export default function Formulario() {
+
+  const times = [
+    'Back-end',
+     'Front-end',
+     'Data Science',
+      'Devops',
+      'UX e Design',
+      'Mobile',
+      "Inovação e Gestão"
+
+   ]
+
   return (
     <section className="formulario">
       <form>
-        <h2>Preencha os dados para criar o card do colaborador.</h2>
+        <h2>Preencha os dados para criar o card do Funcionario.</h2>
         <CampoTexto label="Nome" placeholder="Digite seu nome" />
         <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
         <CampoTexto label="Imagem" placeholder="Informe o endereço da imagem" />
+        <ListaSuspensa label="Times" itens={times}/>
       </form>
     </section>
   );
