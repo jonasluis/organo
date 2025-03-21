4b783e240fc9f76cbd489be4af330d5def6abf7a
jonasluis <jonasluis66@gmail.com>
2025-03-21 12:31:50 -0300

    :sparkles: feat:  id nos times

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import Banner from './componentes/Banner/index.js';
 import Formulario from './componentes/Formulario/index.js';
 import Time from './componentes/Time/index.js';
@@ -8,34 +9,42 @@ function App() {
 
   const [times, setTimes] = useState([
     {
+      id: uuidv4(),
       nome: 'Back-end',
       cor: '#D9F7E9'
     },
     {
+      id: uuidv4(),
       nome: 'Front-end',
       cor: '#E8F8FF'
     },
     {
+      id: uuidv4(),
       nome: 'Full-Stack',
       cor: '#F0F8E2'
     },
     {
+      id: uuidv4(),
       nome: 'Data Science',
       cor: '#FDE7E8'
     },
     {
+      id: uuidv4(),
       nome: 'Devops',
       cor: '#FAE9F5'
     },
     {
+      id: uuidv4(),
       nome: 'UX e Design',
       cor: '#FFF5D9'
     },
     {
+      id: uuidv4(),
       nome: 'Mobile',
       cor: '#FFEEDF'
     },
     {
+      id: uuidv4(),
       nome: 'Inovação e Gestão',
       cor: '#d7c1ec'
     }
@@ -52,9 +61,9 @@ function App() {
 
   }
 
-  function mudarCorDoTime(cor, nome){
+  function mudarCorDoTime(cor, id){
     setTimes(times.map(time => {
-      if(time.nome === nome){
+      if(time.id === id){
         time.cor = cor;
         return { ...time, cor: cor };
       }
--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -9,7 +9,7 @@ export default function Time({ time, colaboradores, aoDeletar, mudarCor }) {
         colaboradores.length > 0 ? (
             <section className='time' style={css}>
                 <input 
-                    onChange={evento => mudarCor(evento.target.value, time.nome)} 
+                    onChange={evento => mudarCor(evento.target.value, time.id)} 
                     value={time.cor} 
                     type='color' 
                     className='input-cor'
