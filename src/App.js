6a8878512493b5fd175a4bb16b7b1e6e01423423
jonasluis <jonasluis66@gmail.com>
2025-03-21 12:15:00 -0300

    :sparkles: feat:  mudando as cores dos times de forma dinamica

--- src/App.js
+++ src/App.js
@@ -6,48 +6,40 @@ import Rodape from './componentes/Rodape/index.js';
 
 function App() {
 
-  const times = [
+  const [times, setTimes] = useState([
     {
       nome: 'Back-end',
-      corPrimaria: '#57C278',
-      corSecundaria: '#D9F7E9'
+      cor: '#D9F7E9'
     },
     {
       nome: 'Front-end',
-      corPrimaria: '#82CFFA',
-      corSecundaria: '#E8F8FF'
+      cor: '#E8F8FF'
     },
     {
       nome: 'Full-Stack',
-      corPrimaria: '#A6D157',
-      corSecundaria: '#F0F8E2'
+      cor: '#F0F8E2'
     },
     {
       nome: 'Data Science',
-      corPrimaria: '#E06B69',
-      corSecundaria: '#FDE7E8'
+      cor: '#FDE7E8'
     },
     {
       nome: 'Devops',
-      corPrimaria: '#DB6EBF',
-      corSecundaria: '#FAE9F5'
+      cor: '#FAE9F5'
     },
     {
       nome: 'UX e Design',
-      corPrimaria: '#FFBA05',
-      corSecundaria: '#FFF5D9'
+      cor: '#FFF5D9'
     },
     {
       nome: 'Mobile',
-      corPrimaria: '#FF8A29',
-      corSecundaria: '#FFEEDF'
+      cor: '#FFEEDF'
     },
     {
       nome: 'Inovação e Gestão',
-      corPrimaria: '#9d57c2',
-      corSecundaria: '#d7c1ec'
+      cor: '#d7c1ec'
     }
-  ]
+  ])
 
   const [colaboradores, setColaboradores] = useState([])
 
@@ -60,6 +52,16 @@ function App() {
 
   }
 
+  function mudarCorDoTime(cor, nome){
+    setTimes(times.map(time => {
+      if(time.nome === nome){
+        time.cor = cor;
+        return { ...time, cor: cor };
+      }
+      return time
+    }))
+  }
+
   return (
     <div className="App">
       <Banner/>
@@ -68,10 +70,9 @@ function App() {
           <h1>Minha organização</h1>
       {times.map(time => 
         <Time 
+          mudarCor={mudarCorDoTime}
           key={time.nome} 
-          nome={time.nome} 
-          corPrimaria={time.corPrimaria} 
-          corSecundaria={time.corSecundaria}
+          time={time}
           colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)}
           aoDeletar ={aoDeletarColaborador}
         />)
--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -1,20 +1,33 @@
+import hexToRgba from 'hex-to-rgba';
 import Colaborador from '../Colaborador'
 import './index.css'
 
-export default function Time(props, aoDeletar){
-    const css = {backgroundColor: props.corSecundaria}
+export default function Time({ time, colaboradores, aoDeletar, mudarCor }) {
+    const css = { backgroundColor: hexToRgba(time.cor, '0.6') };
+
     return (
-
-
-        (props.colaboradores.length > 0) ? <section className='time' style={css}>
-            <h3 style={{borderColor: props.corPrimaria}}>{props.nome}</h3>
-            <div className='colaboradores'>
-                {props.colaboradores.map( colaborador => {
-                   return <Colaborador corDeFundo={props.corPrimaria} key={colaborador.nome} nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem} aoDeletar={aoDeletar}/>
-                })}
-            </div>
-        </section>
-        : ''
-    )
-
+        colaboradores.length > 0 ? (
+            <section className='time' style={css}>
+                <input 
+                    onChange={evento => mudarCor(evento.target.value, time.nome)} 
+                    value={time.cor} 
+                    type='color' 
+                    className='input-cor'
+                />
+                <h3 style={{ borderColor: time.cor }}>{time.nome}</h3>
+                <div className='colaboradores'>
+                    {colaboradores.map(colaborador => (
+                        <Colaborador 
+                            corDeFundo={time.cor} 
+                            key={colaborador.nome} 
+                            nome={colaborador.nome} 
+                            cargo={colaborador.cargo} 
+                            imagem={colaborador.imagem} 
+                            aoDeletar={aoDeletar} 
+                        />
+                    ))}
+                </div>
+            </section>
+        ) : null
+    );
 }
